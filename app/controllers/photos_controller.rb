fe4b14b5565795ab8903ac7489b5b510902bf829
dlondon4 <dlondon4@gmail.com>
2014-11-06 13:02:51 -0600

    Photo Details page working and pulling in data

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,11 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @list_of_photos = Photo.all
+    @photo_id = params[:id]
+    # @p = @list_of_photos.where({ :id => "@photo_id"})
+    @p = @list_of_photos.find(@photo_id)
+  end
 
 end
