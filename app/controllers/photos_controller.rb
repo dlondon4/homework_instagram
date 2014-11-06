c6c748055e48624f666f0b339a19fb4e0746b70f
dlondon4 <dlondon4@gmail.com>
2014-11-06 14:24:22 -0600

    Update complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -29,4 +29,23 @@ class PhotosController < ApplicationController
     @p.destroy
   end
 
+  def edit_form
+    @list_of_photos = Photo.all
+    @photo_id = params[:id]
+    @p = @list_of_photos.find(@photo_id)
+  end
+
+  def update_row
+    @list_of_photos = Photo.all # retrieve the corresponding row from the table
+    @photo_id = params[:id]
+    @p = @list_of_photos.find(@photo_id)
+
+    @change_caption = params[:the_caption] # receive data from an edit form
+    @change_photo = params[:the_source]
+
+    @p.source = @change_photo
+    @p.caption = @change_caption
+    @p.save
+  end
+
 end
